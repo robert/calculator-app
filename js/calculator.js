f7fb980ff00d4c21b290e84cc11118b69ed86194
robert <robqheaton@gmail.com>
2021-08-22 09:58:56 +0100

    wip

--- js/calculator.js
+++ js/calculator.js
@@ -7,31 +7,32 @@ const equalsBtn = document.getElementById("equals-btn")
 
 
 class Calculator { // Create calculator class
-    constructor(calculatorDisplay){
-        this.calculatorDisplay = calculatorDisplay
+
+    constructor(){
+        this.displayValue = ""
         this.operation = [] // Create empty array to store operation in
         this.answer = 0
     }
 
-    displayInput(number) {
-        this.calculatorDisplay.textContent += number //Display number entered on screen
+    appendInput(number) {
+        this.displayValue += number //Display number entered on screen
     }
 
     getOperation(operator) {
-        if(this.calculatorDisplay.textContent !== "" && this.calculatorDisplay.textContent !== ".") { //check if user has entered a valid number
-            this.operation.push(this.calculatorDisplay.textContent) // Push number entered to operation array
+        if(this.displayValue !== "" && this.displayValue !== ".") { //check if user has entered a valid number
+            this.operation.push(this.displayValue) // Push number entered to operation array
             this.operation.push(operator) // Push operator entered to opreation array
             this.clearInput() // Clear calculator screen
         }
     }
 
     clearInput() {
-        this.calculatorDisplay.textContent = ""
+        this.displayValue = ""
     }
 
     delete() {
-        if(this.calculatorDisplay.textContent !== "") {
-            subStr = this.calculatorDisplay.textContent // Create variable with calculators current value
+        if(this.displayValue !== "") {
+            subStr = this.displayValue // Create variable with calculators current value
             subStr = this.subStr.substring(0, subStr.length -1) // Remove last char from string
             this.calculatorDisplay.textContent = subStr // Display new value
         }
@@ -44,7 +45,7 @@ class Calculator { // Create calculator class
 
 
     preformOperation() {
-        this.operation.push(this.calculatorDisplay.textContent) // Get last value entered
+        this.operation.push(this.displayValue) // Get last value entered
         this.answer = parseFloat(this.operation[0]) // Set answer to equal first number in operation array
 
         if(this.operation.length >= 3) {
@@ -65,34 +66,43 @@ class Calculator { // Create calculator class
                     break
                 }
             }
-            this.calculatorDisplay.textContent = this.answer  // Display answer once for loop has finished
+            this.displayValue = this.answer  // Display answer once for loop has finished
             this.operation = [] // set operation array back to and empty array
         }
     }
 }
 
-const calculator = new Calculator(calculatorDisplay)
+const calculator = new Calculator()
 
 for(let btn of numBtns) { // Add event listner to each number btn
     btn.addEventListener("click", () => {
-        calculator.displayInput(btn.textContent) // Display btns text content when clicked
-    } )
+        calculator.appendInput(btn.textContent) // Display btns text content when clicked
+        calculatorDisplay.textContext = calculator.displayValue
+        console.log(calculatorDisplay.textContent)
+        console.log(calculator.displayValue)
+    })
 }
 
 for(let btn of operatorBtns) { // Add event listner to each operator btn
     btn.addEventListener("click", () => {
         calculator.getOperation(btn.textContent) // Run calculators get operation function when clicked
+        calculatorDisplay.textContext = calculator.displayValue
     })
 }
 
 deleteBtn.addEventListener("click", () => {
     calculator.delete()
+    calculatorDisplay.textContext = calculator.displayValue
 })
 
 resetBtn.addEventListener("click", (e) => {
     calculator.resetAll()
+    calculatorDisplay.textContext = calculator.displayValue
 })
 
 equalsBtn.addEventListener("click", () => {
     calculator.preformOperation()
+    calculatorDisplay.textContext = calculator.displayValue
 })
+
+console.log("HI")
